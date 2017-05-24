711a9e3ff22871b3fc614b98aa5bc165e2d5577f
cwright <cwright@renewfund.com>
2017-05-24 09:28:26 -0700

    fix spec and readme

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ To get started, from command line:
 1. change into project directory with `cd tic_tac_toe`
 1. install rspec gem with `gem install rspec`
 1. run test suite with `rspec spec`
+
+
+To play the game:
+`ruby run.rb`
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -2,7 +2,8 @@ require_relative "../spec_helper.rb"
 require_relative "../tic_tac_toe.rb"
 
 RSpec.describe TicTacToe do
-  subject { described_class.new }
+  subject { described_class.new(player_one: "foo", player_two: "bar") }
+
   it "works" do
     expect(subject).to be_a TicTacToe
   end
